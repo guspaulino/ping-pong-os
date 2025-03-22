fc3481221469fb82a876e1c9d4545451a9ce7120
Gustavo Vinícius Paulino <gustavo.vinicius.paulino@gmail.com>
2025-03-22 18:41:31 -0300

    add queue and testafila files

---
+++ queue.c
@@ -0,0 +1,9 @@
+#include "queue.h"
+
+int queue_size (queue_t *queue){}
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*) ){}
+
+int queue_append (queue_t **queue, queue_t *elem){}
+
+int queue_remove (queue_t **queue, queue_t *elem){}
